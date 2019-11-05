0dbd756886ab22831c696c0f478ae09cb0171a92
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2019-11-06 00:53:04 +0900

    Add ga

--- packages/frontend/src/main.js
+++ packages/frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import VueAnalytics from 'vue-analytics';
 import locale from 'element-ui/lib/locale/lang/ja';
 
 import App from './App.vue';
@@ -9,6 +10,10 @@ import store from './store';
 import activateLink from './plugins/tweet';
 
 Vue.use(ElementUI, { locale });
+Vue.use(VueAnalytics, {
+  id: 'UA-42893827-25',
+  router
+});
 
 Vue.prototype.$activateLink = activateLink;
 
