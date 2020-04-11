bdec02cf4867e1b872985fb5bab6e734cb859716
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-04-11 19:54:39 +0900

    Use host validation

--- packages/backend/express.js
+++ packages/backend/express.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
 const compression = require('compression');
+const hostValidation = require('host-validation');
 
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
@@ -19,6 +20,17 @@ module.exports = () => {
   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
   app.use(methodOverride());
   app.use(compression({ level: 9 }));
+  app.use(
+    hostValidation({
+      hosts: [
+        '127.0.0.1:8100',
+        'localhost:8100',
+        '127.0.0.1:9100',
+        'localhost:9100',
+        'mukuu.herokuapp.com',
+      ],
+    }),
+  );
 
   if (env === 'development') {
     require('./config/development')(app);
