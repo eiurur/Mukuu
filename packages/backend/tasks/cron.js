20321d1b6658523e9bc423cea16f4f6b8689d0f9
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-04-16 02:13:44 +0900

    Aviod error that stdout maxBuffer exceeded.

--- packages/backend/tasks/cron.js
+++ packages/backend/tasks/cron.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const CronJob = require('cron').CronJob;
 const { promisify } = require('util');
-const { execFile } = require('child_process');
+const { execFile, spawn } = require('child_process');
 const execFileAsync = promisify(execFile);
 const logger = require(path.join('..', 'logger'))('cron');
 
 const runProcess = async (filepath) => {
   const { stdout, stderr } = await execFileAsync('node', [filepath], {
-    maxBuffer: 1024 * 1024 * 10,
+    maxBuffer: 1024 * 1024 * 100,
     shell: true,
   });
   if (stderr) {
@@ -17,27 +17,53 @@ const runProcess = async (filepath) => {
   return stdout;
 };
 
+const spawnProcess = (cmd, args = [], option = {}) => {
+  return new Promise((resolve, reject) => {
+    const normalizedCmd = cmd.replace(/\\/g, '/');
+    const normalizedArgs = args.map((arg) => arg.replace(/\\/g, '/'));
+    let stdoutChunks = [];
+    let stderrChunks = [];
+    const child = spawn(normalizedCmd, normalizedArgs);
+    child.stdout.on('data', (data) => {
+      stdoutChunks = stdoutChunks.concat(data);
+    });
+    child.stdout.on('end', () => {
+      const stdoutContent = Buffer.concat(stdoutChunks).toString();
+      console.log('stdout chars:', stdoutContent.length);
+      console.log(stdoutContent);
+      return resolve(stdoutContent);
+    });
+
+    child.stderr.on('data', (data) => {
+      stderrChunks = stderrChunks.concat(data);
+    });
+    child.stderr.on('end', () => {
+      const stderrContent = Buffer.concat(stderrChunks).toString();
+      console.log('stderr chars:', stderrContent.length);
+      console.log(stderrContent);
+      return reject(stderrContent);
+    });
+  });
+};
+
 const tweetJob = new CronJob({
   // cronTime: '* * * * *',
-  cronTime: '0 0,9,12,15,18,20,23 * * *',
+  cronTime: '0 0,9,12,15,18,20,22 * * *',
   onTick: async () => {
     logger.info('--- start tweet cron ---');
-    await Promise.all([
-      runProcess(path.resolve(__dirname, 'tweet', 'tweetCrawling')),
-    ]);
+    const args = [path.resolve(__dirname, 'tweet', 'tweetCrawling')];
+    const stdout = await spawnProcess('node', args);
     logger.info('--- finish tweet cron ---');
   },
   start: true,
   timeZone: 'Asia/Tokyo',
 });
 const userJob = new CronJob({
-  // cronTime: '* * * * *',
   cronTime: '0 13,21 * * *',
   onTick: async () => {
     logger.info('--- start user cron ---');
-    await Promise.all([
-      runProcess(path.resolve(__dirname, 'tweet', 'dailyUserCrawling')),
-    ]);
+    const args = [path.resolve(__dirname, 'tweet', 'dailyUserCrawling')];
+    const stdout = await spawnProcess('node', args);
     logger.info('--- finish user cron ---');
   },
   start: true,
@@ -45,12 +71,11 @@ const userJob = new CronJob({
 });
 const databaseJob = new CronJob({
   // cronTime: '* * * * *',
-  cronTime: '0 14,22 * * *',
+  cronTime: '0 14,23 * * *',
   onTick: async () => {
     logger.info('--- start database cron ---');
-    await Promise.all([
-      runProcess(path.resolve(__dirname, 'database', 'updateDatabase.js')),
-    ]);
+    const args = [path.resolve(__dirname, 'tweet', 'updateDatabase')];
+    const stdout = await spawnProcess('node', args);
     logger.info('--- finish database cron ---');
   },
   start: true,
