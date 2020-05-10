d912d3909ae425e3dd45024ec05f4c4c7738e384
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-05-10 22:54:20 +0900

    Stop dynamic refererence of $route.path on router

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,32 +1,7 @@
 <template>
   <div id="app">
     <aside class="el-aside">
-      <el-menu
-        :router="true"
-        :default-active="$route.path"
-        :mode="$mq == 'sm' ? 'horizontal' : 'vertical'"
-      >
-        <el-menu-item index="/" :route="{ name: 'home' }">
-          <i class="el-icon-s-home"></i>
-          <span slot="title">Home</span>
-        </el-menu-item>
-        <el-menu-item index="/users" :route="{ name: 'users' }">
-          <i class="el-icon-user-solid"></i>
-          <span slot="title">Users</span>
-        </el-menu-item>
-        <el-menu-item index="/watch" :route="{ name: 'watch' }">
-          <i class="el-icon-view"></i>
-          <span slot="title">Watch</span>
-        </el-menu-item>
-        <el-menu-item index="/bookmark" :route="{ name: 'bookmark' }">
-          <i class="el-icon-collection-tag"></i>
-          <span slot="title">Bukuma</span>
-        </el-menu-item>
-        <el-menu-item index="/about">
-          <i class="el-icon-info"></i>
-          <span slot="title">About</span>
-        </el-menu-item>
-      </el-menu>
+      <Menu></Menu>
     </aside>
     <main v-cloak>
       <router-view />
@@ -34,27 +9,21 @@
   </div>
 </template>
 
+<script>
+import Menu from "@/container/Menu.vue";
+
+export default {
+  name: "App",
+  components: {
+    Menu
+  }
+};
+</script>
 <style lang="scss">
 @import "@/styles/index.scss";
 
 main {
   width: 100%;
   padding: 0.5rem;
-}
-.sb {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: flex-end;
-  border-radius: 4px;
-  bottom: 0;
-  a {
-    width: 100%;
-  }
-  img {
-    width: 100%;
-    display: flex;
-    align-items: flex-end;
-  }
 }
 </style>
---
+++ packages/frontend/src/container/Menu.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-menu
+    :router="true"
+    :default-active="activeIndex"
+    :mode="$mq == 'sm' ? 'horizontal' : 'vertical'"
+  >
+    <el-menu-item index="home" :route="{ name: 'home' }">
+      <i class="el-icon-s-home"></i>
+      <span slot="title">Home</span>
+    </el-menu-item>
+    <el-menu-item index="users" :route="{ name: 'users' }">
+      <i class="el-icon-user-solid"></i>
+      <span slot="title">Users</span>
+    </el-menu-item>
+    <el-menu-item index="watch" :route="{ name: 'watch' }">
+      <i class="el-icon-view"></i>
+      <span slot="title">Watch</span>
+    </el-menu-item>
+    <el-menu-item index="bookmark" :route="{ name: 'bookmark' }">
+      <i class="el-icon-collection-tag"></i>
+      <span slot="title">Bukuma</span>
+    </el-menu-item>
+    <el-menu-item index="about">
+      <i class="el-icon-info"></i>
+      <span slot="title">About</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+
+  data() {
+    return {
+      activeIndex: ""
+    };
+  },
+  mounted() {
+    this.activeIndex = this.$route.name;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sb {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  border-radius: 4px;
+  bottom: 0;
+  a {
+    width: 100%;
+  }
+  img {
+    width: 100%;
+    display: flex;
+    align-items: flex-end;
+  }
+}
+</style>
