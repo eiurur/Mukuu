6f4ee88009c3ee181605c7d7648572e0c09572ba
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-05-10 11:20:23 +0900

    Optimize performance

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -1,11 +1,17 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:9100/'
+      "/api": {
+        target: "http://127.0.0.1:9100/"
       }
     },
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 8100
+  },
+  chainWebpack(config) {
+    config.plugins.delete("prefetch");
+    config.plugin("CompressionPlugin").use(CompressionPlugin);
   }
 };
