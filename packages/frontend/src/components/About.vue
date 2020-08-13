9155a12344a32092bd0a1513d22382c667ca2a14
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-08-14 07:17:41 +0900

    update about

--- packages/frontend/src/components/About.vue
+++ packages/frontend/src/components/About.vue
@@ -6,9 +6,16 @@
         <section>
           <p>
             Mukuu は
-            <a href="http://com3d2.jp/" target="_blank">カスタムオーダーメイド3D2(COM3D2)</a>のMODの検索を効率化するためのサイトです。
+            <a href="http://com3d2.jp/" target="_blank">カスタムオーダーメイド3D2(COM3D2)</a
+            >のMODの検索を効率化するためのサイトです。
           </p>
-          <p>気になったMODの製作者様の過去作品を辿る手段が煩雑だった問題を解消するために作りました。</p>
+          <p>
+            気になったMODの製作者様の過去作品を辿る手段が煩雑だった問題を解消するために作りました。
+          </p>
+          <p>
+            収集および表示対象は ux.getuploader.com / drive.google.com / mega.nz
+            のいずれかを含むツイートとなっています。
+          </p>
         </section>
         <h1>外部リンク</h1>
         <section>
