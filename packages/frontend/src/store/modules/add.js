d03694b473f55c1886be7b64f6b45d9ad7656b93
eiurur <eiurur789@gmail.com>
2022-10-19 10:19:11 +0900

    add adds

--- packages/frontend/src/store/modules/add.js
+++ packages/frontend/src/store/modules/add.js
@@ -13,6 +13,8 @@ const actions = {
     const day = times().format("YYYY-MM-DD");
     const conditions = [
       `/api/v1/adds?term=days&range=new&type=maniax&category=voice&sub=SOU&aid=maidimaid&day=${day}`,
+      `/api/v1/adds?term=days&range=new&type=maniax&category=voice&sub=SOU&aid=maidimaid&day=${day}`,
+      `/api/v1/adds?term=days&range=new&type=maniax&category=voice&sub=SOU&aid=maidimaid&day=${day}`,
       `/api/v1/adds?term=days&range=new&type=maniax&category=game&aid=maidimaid&day=${day}`,
       `/api/v1/adds?term=days&range=new&type=maniax&category=comic&aid=maidimaid&day=${day}`,
       `/api/v1/adds?term=days&range=new&type=maniax&aid=maidimaid&day=${day}`,
