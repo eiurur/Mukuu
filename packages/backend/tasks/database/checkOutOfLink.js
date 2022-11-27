0b6787f69091bb41dadecb4e224c1e41cbeb18e7
eiurur <eiurur789@gmail.com>
2022-11-27 23:55:03 +0900

    #124 - add progress and fix mistake of registration of out of link

--- packages/backend/tasks/database/checkOutOfLink.js
+++ packages/backend/tasks/database/checkOutOfLink.js
@@ -15,38 +15,34 @@ const { sleep } = require('../../lib/utils');
     let skip = 0;
     while (true) {
       const query = {};
-      const searchOption = { isOutOfLink: false, limit, skip };
+      const searchOption = { isOutOfLink: false,
+        limit,
+        skip,
+        sort: 'createdAtDesc', };
       const posts = await postProvider.find(query, searchOption);
       if (!posts || posts.length <= 0) break;
       for (const post of posts) {
         const entities = JSON.parse(post.entities);
-        if (entities && entities.urls) {
-          for (const urls of entities.urls) {
-            try {
-              console.log(urls.expanded_url);
-              if (urls.expanded_url.indexOf("https://mega.nz") !== -1) {
-                await setOutOfLink(post, false);
-                continue; // FREEZEするのでスキップ
-              }
-              const { status } = await axios.head(urls.expanded_url, {
-                headers: {
-                  referer: urls.expanded_url,
-                }
-              });
-              await setOutOfLink(post, false);
-              console.log(status);
-            } catch (e) {
-              console.log(e.response && e.response.status);
-              if (e.response && e.response.status === 404) {
-                await setOutOfLink(post, true);
-              }
-            }
-            await sleep(500);
+        if (!entities || !entities.urls) continue;
+        try {
+          const results = await Promise.all(entities.urls.map(urls => {
+            console.log(urls.expanded_url, post.createdAt);
+            if (urls.expanded_url.indexOf("https://mega.nz") !== -1) return true;
+            return axios.head(urls.expanded_url);
+          }));
+          await setOutOfLink(post, false);
+        } catch (e) {
+          console.log(e.response && e.response.status);
+          if (e.response && e.response.status === 404) {
+            await setOutOfLink(post, true);
           }
         }
+        await sleep(500);
       }
       skip += limit;
+      console.log(skip);
     }
+    console.log("FIN");
   } catch (e) {
     logger.info(e);
   }
