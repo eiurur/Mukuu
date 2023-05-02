6efb76bca3e6f35907d21401623fd357722bb9c1
eiurur <eiurur789@gmail.com>
2023-05-02 20:30:14 +0900

    register user images  to db

--- packages/backend/models/schemas/user.js
+++ packages/backend/models/schemas/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
 
 const UserSchema = new Schema({
   idStr: {
@@ -29,6 +29,8 @@ const UserSchema = new Schema({
   profileBackgroundImageUrl: String,
   profileImageUrl: String,
   profileBannerUrl: String,
+  profileImageUrlBase64: String,
+  profileBannerUrlBase64: String,
   createdAt: {
     type: Date,
     default: Date.now,
